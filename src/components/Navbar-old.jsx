23ffd09ae7cad2160fbb66fe793392629ce0ffdd
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2024-03-02 23:53:46 +0530

    project section complete

--- src/components/Navbar-old.jsx
+++ src/components/Navbar-old.jsx
@@ -4,7 +4,7 @@ import logo from '../assets/ags-new.png'
 
 const Navbar = () => {
   return (
-    <nav className="flex w-full fixed sm:h-[6vh] md:h-[6vh] h-[70px] bg-[#1B383A]/80 text-white justify-evenly items-center">
+    <nav className="flex w-full fixed sm:h-[6vh] md:h-[6vh] h-[70px] bg-[#1B383A] text-white justify-evenly items-center">
         <img src={logo} alt="logo" className="w-[3rem] h-[35px] object-contain "/>
         <ul className="flex justify-around items-center gap-3 ">
             <li><a href="#">Home</a></li>
