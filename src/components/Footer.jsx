048d7451adfda80efd4a6cf6f75edad2a29a0db9
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2024-03-03 22:20:23 +0530

    footer border removed

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -8,8 +8,8 @@ import { FaSquareWhatsapp } from "react-icons/fa6";
 const Footer = () => {
   return (
     <div className='bg-[#091e20] text-white md:flex justify-center items-center md:h-[50vh] py-2 w-full'>
-      <div className='max-w-[1440px] h-[full] md:flex border border-white justify-between  mx-auto  p-1 m-1 gap-2'>
-        <div className='md:w-[40rem]  md:p-3 p-1 border border-white'>
+      <div className='max-w-[1440px] h-[full] md:flex  justify-between mx-auto p-1 m-1 gap-2'>
+        <div className='md:w-[40rem] md:p-3 p-1'>
             <h2>Ags Sofas</h2>
             <div className='flex flex-wrap items-center gap-2 p-1'>
                 <LuClock9  size={30}/>
@@ -24,19 +24,13 @@ const Footer = () => {
                 <p>Opp Margosa Heights Apartment, Mohammadwadi, Hadapsar, Pune, Maharashtra 411028</p>
             </div>
         </div>
-        <div className='md:w-[40rem]  md:p-3 p-1 border border-white'>
+        <div className='md:w-[40rem]  md:p-3 p-1'>
             <h2 className='mx-1'>Follow us </h2>
             <div className='flex items-center gap-2 p-1'>
             <FaSquareWhatsapp  size={40}  className='cursor-pointer'/>
             <FaInstagram  size={40} className='cursor-pointer'/>
             <FaFacebookSquare  size={40} className='cursor-pointer'/>
             </div>
-            {/* <div className='flex items-center gap-2 p-1'>
-                
-            </div>
-            <div className='flex items-center gap-2 p-1'>
-                
-            </div> */}
         </div>
       </div>
     </div>
