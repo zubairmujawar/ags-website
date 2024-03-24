de6c9c6387a43eab9f3ee3a42e64fac9ceb5adbe
Zubair Mujawar <mujawarzubair167@gmail.com.com>
2024-03-24 20:15:07 +0530

    client chages phone number ect

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -5,6 +5,7 @@ import { FaLocationDot } from "react-icons/fa6";
 import { FaFacebookSquare } from "react-icons/fa";
 import { FaInstagram } from "react-icons/fa";
 import { FaSquareWhatsapp } from "react-icons/fa6";
+import { Link } from 'react-router-dom';
 const Footer = () => {
   return (
     <div className='bg-[#091e20] text-white md:flex justify-center items-center md:h-[50vh] py-2 w-full'>
@@ -25,12 +26,16 @@ const Footer = () => {
                 <p>9 AM to 10 PM</p>
             </div>
             <div className='flex items-center gap-2 p-1'>
+                <Link href="tel:+917020083904" className='flex my-2 gap-3'>
                 <FaPhoneAlt  size={25}/>
                 <p>+91 7020083904</p>
+                </Link>
             </div>
-            <div className='flex items-center gap-2 p-1'>
+            <div className='flex items-center gap-2 p-1 '>
+                <Link href="https://www.google.com/maps/dir/18.474021,73.8771897/AGS+SOFAS,+Opp+Margosa+Heights+Apartment,+Mohammadwadi,+Hadapsar,+Pune,+Maharashtra+411028/@18.4757308,73.8573259,13z/data=!3m1!4b1!4m9!4m8!1m1!4e1!1m5!1m1!1s0x3bc2eb7b7fccff45:0xb285b14d13d7e52f!2m2!1d73.9230623!2d18.4709791?entry=ttu" className='flex my-2 gap-3'>
                 <FaLocationDot size={30} className='mr-2'/>
                 <p>Opp Margosa Heights Apartment, Mohammadwadi, Hadapsar, Pune, Maharashtra 411028</p>
+                </Link>
             </div>
         </motion.div>
         <motion.div className='md:w-[50%]  md:p-3 p-1'
@@ -45,13 +50,13 @@ const Footer = () => {
         >
             <h2 className='mx-1'>Follow us </h2>
             <div className='flex items-center gap-2 p-1'>
-            <a href="whatsapp://send?phone=+919579569517&text=Hello!" target="_blank">
+            <a href="whatsapp://send?phone=+917020083904&text=Hi there! 👋 I'm interested in learning more about your ags sofa. Could you please provide me with additional details? Thank you! 🛋️" target="_blank">
             <FaSquareWhatsapp  size={40}  className='cursor-pointer hover:text-green-900'/>
             </a>
             <a href="https://instagram.com/agssofas" target="_blank">
             <FaInstagram  size={40} className='cursor-pointer hover:text-green-900'/>
             </a>
-            <a href="https://facebook.com/agssofas" target="_blank">
+            <a href="https://www.facebook.com/people/Ags-Sofas/pfbid0hEAYNQFJG6VnePEz5Djt8kR2svrhf6T1XDsPuUbnxMqMGLzgaeexUV3ukJi4eY63l/" target="_blank">
             <FaFacebookSquare  size={40} className='cursor-pointer hover:text-green-900'/>
             </a>
             </div>
--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import React, { useRef, useEffect, useState } from "react";
 import projectImg1 from "../assets/projectImg1.png";
 import projectImg2 from "../assets/projectImg2.png";
 import video from "../assets/video.mp4";
@@ -20,9 +21,46 @@ const HeroSection = () => {
     },
   };
 
-    const scrollToTop = () => {
-      window.scrollTo({ top: 0, behavior: 'smooth' });
+  const scrollToTop = () => {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  };
+
+  const contentRef = useRef(null);
+  const [isVisible, setIsVisible] = useState(false);
+
+  const isInViewport = () => {
+    const rect = contentRef.current.getBoundingClientRect();
+    return (
+      rect.top >= 0 &&
+      rect.left >= 0 &&
+      rect.bottom <=
+        (window.innerHeight || document.documentElement.clientHeight) &&
+      rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+  };
+
+  const handleScroll = () => {
+    setIsVisible(isInViewport());
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+    return () => {
+      window.removeEventListener("scroll", handleScroll);
     };
+  }, []);
+
+  useEffect(() => {
+    if (isVisible) {
+      const content = contentRef.current;
+      content.classList.add("fade-in");
+    }
+    // Remove fade-in class when component unmounts
+    return () => {
+      const content = contentRef.current;
+      content.classList.remove("fade-in");
+    };
+  }, [isVisible]);
 
   return (
     <>
@@ -97,6 +135,34 @@ const HeroSection = () => {
             unique personality and celebrates the art of living beautifully."
           </motion.p>
         </motion.div>
+      </div>
+      {/* Product section */}
+      <div className="w-full min-h-full flex justify-center items-center relative">
+        <img
+          src={
+            "https://images.pexels.com/photos/1571460/pexels-photo-1571460.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+          }
+          alt="heroImg"
+          className="w-full h-[80vh] md:object-cover object-cover opacity-[0.8]"
+        />
+        <div
+          ref={contentRef}
+          className="absolute flex flex-col gap-3 justify-center items-center opacity-0"
+        >
+          <h1 className="md:text-6xl text-3xl text-black font-bold">
+            Home decor items
+          </h1>
+          <h4 className="md:text-4xl text-2xl text-black font-semibold">
+            Find your new favorites
+          </h4>
+          <div className="flex justify-between items-center">
+            <Link to="/products">
+            <button className="bg-transparent hover:bg-[#C1A362] rounded-full text-black font-semibold hover:text-white py-2 px-4 border border-[#262115] hover:border-transparent">
+              Shop now
+            </button>
+            </Link>
+          </div>
+        </div>
       </div>
       {/* Projects section */}
       <div className="w-full min-h-[80vh] project-div py-2 md:flex justify-center items-center">
